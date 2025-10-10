9f83e93ab6b4a69a7a1a2bd0642eaaeba7ec47d3
Nikola Ratkovac <nikolaratkovac3@gmail.com>
2025-10-10 22:50:04 +0200

    initial commit

---
+++ src/main/java/com/stakeholders/StakeholdersApplication.java
@@ -0,0 +1,13 @@
+package com.stakeholders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StakeholdersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StakeholdersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stakeholders/StakeholdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stakeholders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StakeholdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
